2c5d8b11e59fe17025caa2d01f269c1061f9ad5e
NonoshFather <tkibarsoy123@gmail.com>
2022-01-13 18:56:49 +0300

    ,

--- mainprog.py
+++ mainprog.py
@@ -1,9 +1,9 @@
 import tkinter as tk
 import fnmatch
 import os
-from tkinter.constants import  ACTIVE, ANCHOR, BOTTOM, E, END, GROOVE, HORIZONTAL, W, X
+from tkinter.constants import  ACTIVE, ANCHOR, BOTTOM, END, GROOVE, HORIZONTAL, VERTICAL, W, X
 from pygame import mixer
-from tkinter import Label, filedialog
+from tkinter import Frame, Label, filedialog
 import time
 from mutagen.mp3 import MP3
 import tkinter.ttk as ttk
@@ -15,7 +15,7 @@ import pygame
 root = tk.Tk()
 root.title("Casette MP3 Çalar")
 root.iconbitmap('casettev2.ico')
-root.geometry("640x480")
+root.geometry("1280x720")
 root.config(bg = "gray")
 
 pygame.mixer.init()
@@ -179,23 +179,31 @@ def slide(X):
     pygame.mixer.music.load(sarki)
     pygame.mixer.music.play(loops=0, start=int(slider.get()))
 
-liste = tk.Listbox(root, bg="black" , fg="white" ,height= 20, width=160, selectbackground="gray", selectforeground="white")
-liste.pack(pady=20)
+def ses(X):
+    pass
+
+
+master_frame = tk.Frame(root, bg="gray")
+master_frame.pack(pady=20)
+
+liste = tk.Listbox(master_frame, bg="black" , fg="white" ,height= 20, width=140, selectbackground="gray", selectforeground="white")
+liste.grid(row=0, column=0)
 
 geri_t = tk.PhotoImage(file= 'reverse 50x50.png')
 ileri_t = tk.PhotoImage(file= 'forward 50x50.png')
 oynat_t = tk.PhotoImage(file= 'play button 50x50.png')
 durdur_t = tk.PhotoImage(file= 'pause 50x50 remake.png')
 
-kontrl_pan = tk.Frame(root, bg="gray")
-kontrl_pan.pack()
+kontrl_pan = tk.Frame(master_frame, bg="gray")
+kontrl_pan.grid(row=2, column=0, pady=0)
 
 geri = tk.Button(kontrl_pan, bg="gray", image=geri_t, borderwidth= 0, width= 50, height= 50, command= önceki_sarki)
 ileri = tk.Button(kontrl_pan, image=ileri_t, bg= "gray", borderwidth= 0, width= 50, height= 50, command = sonraki_sarki)
 oynat = tk.Button(kontrl_pan, image=oynat_t, bg="gray", borderwidth= 0, width= 50, height= 50, command= play)
 durdur = tk.Button(kontrl_pan, image=durdur_t, bg="gray", borderwidth= 0, width= 50, height= 50, command= lambda: pause (paused))
 
-
+ses_frame = ttk.LabelFrame(root, text="SES DÜZEYİ")
+ses_frame.grid(row=5, column=0)
 geri.grid(row=0, column=0, padx=10)
 ileri.grid(row=0, column=3, padx=10)
 oynat.grid(row=0, column=1, padx=10)
@@ -216,10 +224,14 @@ delete_s_menu.add_command(label="Oynatma listesini temizle", command=liste_temiz
 status_bar = Label(root, text='', bd=0,bg="gray",fg="white", relief=GROOVE, anchor=W)
 status_bar.pack(fill=X, side=BOTTOM, ipady=2)
 
-slider = ttk.Scale(root, from_=0, to=100, orient=HORIZONTAL, value=0, command= slide, length=400)
-slider.pack(pady=5)
+slider = ttk.Scale(master_frame, from_=0, to=100, orient=HORIZONTAL, value=0, command= slide, length=800)
+slider.grid(row=1, column=0,pady=40)
 
 #slider_label = Label(root, text="0")
 #slider_label.pack(pady=10)
+
+ses_slider = ttk.Scale(ses_frame, from_=0, to=1, orient=VERTICAL, value=1, command= ses, length=50)
+slider.pack()
+
 
 root.mainloop()  
